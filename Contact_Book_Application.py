987e408651313123d105432f8c9df07908cbf058
Tridip Karmakar <tridipkarmakar97@gmail.com>
2024-11-01 19:01:17 +0530

    update functionality of dict 1.0

---
+++ Contact_Book_Application.py
@@ -0,0 +1,27 @@
+contacts = {}
+contacts.update("tridip")
+print(contacts)
+'''
+def add_contact(name, phone, email) :
+    contacts[name][phone].update(phone) 
+    contacts[name][phone].update(phone)
+
+'''
+'''
+
+# Adds a new contact.
+add_contact = add_contact(name, phone, email)
+
+# Updates contact details. 
+update_contact = update_contact(name, phone=None, email=None)
+
+# Removes a contact by name. 
+delete_contact = delete_contact(name)
+
+# Views details of a single contact.
+
+view_contact = view_contact(name) 
+
+# Lists all saved contacts. 
+view_all_contacts = view_all_contacts() 
+'''
